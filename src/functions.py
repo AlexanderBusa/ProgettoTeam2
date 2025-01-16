52f4bae249dd52d69dc0808bebb9c0782a7a998e
ghilu91 <ghidelli.luca@gmail.com>
2025-01-16 10:26:30 +0100

    class scalerwrapper to retain column names

--- src/functions.py
+++ src/functions.py
@@ -2,11 +2,56 @@
 #from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #import numpy as np
+
+
+class ScalerWrapper:
+    def __init__(self, scaler, columns):
+        """
+        Initialize the scaler wrapper with a specified scaler.
+        """
+        self.scaler = scaler 
+        self.columns = columns
+
+    def fit_transform(self, X, y = None):
+        """
+        Fit the scaler to the data and transform it, retaining column names.
+        :param X: pd.DataFrame, input data
+        :return: pd.DataFrame, scaled data with original column names
+        """
+        scaled_data = self.scaler.fit_transform(X)
+        return pd.DataFrame(scaled_data, columns=self.columns, index=X.index)
+
+    def transform(self, X):
+        """
+        Transform the data using a previously fitted scaler, retaining column names.
+        :param X: pd.DataFrame, input data
+        :return: pd.DataFrame, scaled data with original column names
+        """
+        scaled_data = self.scaler.transform(X)
+        return pd.DataFrame(scaled_data, columns=self.columns, index=X.index)
+
+    def fit(self, X):
+        """
+        Fit the scaler to the data.
+        :param X: pd.DataFrame, input data
+        """
+        self.scaler.fit(X)
+        return self
+
+    def inverse_transform(self, X):
+        """
+        Inverse transform the scaled data to the original scale.
+        :param X: pd.DataFrame, scaled data
+        :return: pd.DataFrame, original data scale with column names
+        """
+        original_data = self.scaler.inverse_transform(X)
+        return pd.DataFrame(original_data, columns=self.columns, index=X.index)
 
 
 def plot_correlations(df):
